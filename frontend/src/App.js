8ffb0d6f15b204054c6540f2a93ac53e75dbe637
Irfiyandaabidin <irfidupang@gmail.com>
2023-05-10 21:37:54 +0700

    create home page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,9 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Navigation from './components/Navigation';
 import Home from './pages/Home';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import { useState } from 'react';
 
 function App() {
   return (
@@ -12,6 +15,8 @@ function App() {
         <Navigation />
         <Routes>
           <Route path='/' element={<Home/>}/>
+          <Route path='/login' element={<Login/>}/>
+          <Route path='/signup' element={<Signup/>}/>
         </Routes>
       </BrowserRouter>
     </div>
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,8 +1,17 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+      <img src=" https://res.cloudinary.com/learn-code-10/image/upload/v1653947013/yqajnhqf7usk56zkwqi5.png" className="home-banner" />      
+      <div className='featured-products-container container mt-4'>
+        <h2>Last products</h2>
+      </div>
+      <div>
+        <Link to="/category/all" style={{textAlign: "right", display: "block", textDecoration:"none"}}>See more {">>"}</Link>
+      </div>
+    </div>
   )
 }
 
